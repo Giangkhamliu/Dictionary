d97c88e68c6c6c2beea0bc501f4e133df167e6e1
Giangkhamliu <grace21@navgurukul.org>
2022-07-11 12:14:15 +0530

    y first commit

--- length_of_a_given_values.py
+++ length_of_a_given_values.py
@@ -1,7 +1,15 @@
-def test(dictt):
-    result = {}
-    for val in dictt.values(): 
-        result[val] = len(val) 
-    return result    
-color_dict = {'1' : 'Austin Little', '2' : 'Natasha Howard', '3' : 'Alfred Mullins'}
-print(test(color_dict))
+# def test(dictt):
+#     result = {}
+#     for val in dictt.values(): 
+#         result[val] = len(val) 
+#     return result    
+# color_dict = {'1' : 'Austin Little', '2' : 'Natasha Howard', '3' : 'Alfred Mullins'}
+# print(test(color_dict))
+
+
+d={1:["jean castrol"],2:["lula poweii",]} 
+for i in d:
+    for j in d[i]:
+        x=j
+    d[i]=x
+print(d)
